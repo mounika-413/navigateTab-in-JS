958b3b7d3889a1d11dcb63be4337bf90fc4f8301
mounika-413 <tekumalmounika@gmail.com>
2024-11-20 18:59:50 +0530

    this is first commit

---
+++ changeheading.js
@@ -0,0 +1,9 @@
+
+let headingId=document.getElementById("heading");
+
+function manipulateStyles(){
+    headingId.textContent="4.0 technology";
+    headingId.style.color="blue";
+};
+
+console.log(headingId);
